cca289f411c0553745e119bb649b1bd18b4f8cbd
Peklusha <lizapeklo02042002@gmail.com>
2025-01-30 23:18:49 +0300

    внесены доработки

--- read_xlsx_module/EmployeeIterator.py
+++ read_xlsx_module/EmployeeIterator.py
@@ -25,31 +25,34 @@ class GetEmployee:
         Фильтрует строки согласно выбраному отделу и/или управлению
         :return: Список словарей по шаблону как в функции get_employee_by_fio_department
         """
-        department_index = 4
-        admin_index = 0
-        employees = {}
-        for row in self.sheet.iter_rows(min_row=2, values_only=True):
-            if (self.department is None or row[department_index] == self.department)\
+        try:
+            department_index = 4
+            admin_index = 0
+            employees = {}
+            for row in self.sheet.iter_rows(min_row=2, values_only=True):
+                if (self.department is None or row[department_index] == self.department) \
                     and (self.administration is None or row[admin_index] == self.administration):
-                criteria = dict(zip(
-                    [str(i) for i in range(1, 12)],
-                    [float(j.replace(',', '.')) for j in row[6:17] if j is not None]
-                ))
-                results = dict(zip(
-                    ['Год тестирования', 'Критерии', 'Cумма баллов'],
-                    [str(row[18]), criteria, row[17]]
-                ))
-                fio = row[5]
-                if fio not in employees:
-                    employees[fio] = {
-                        'ФИО': fio,
-                        'Должность': row[2],
-                        'Отдел': str(row[4]),
-                        'Результаты': [results]
-                    }
-                else:
-                    employees[fio]['Результаты'].append(results)
-        return list(employees.values())
+                    criteria = dict(zip(
+                        [str(i) for i in range(1, 12)],
+                        [float(j.replace(',', '.')) for j in row[6:17] if j is not None]
+                    ))
+                    results = dict(zip(
+                        ['Год тестирования', 'Критерии', 'Cумма баллов'],
+                        [str(row[18]), criteria, row[17]]
+                    ))
+                    fio = row[5]
+                    if fio not in employees:
+                        employees[fio] = {
+                            'ФИО': fio,
+                            'Должность': row[2],
+                            'Отдел': str(row[4]),
+                            'Результаты': [results]
+                        }
+                    else:
+                        employees[fio]['Результаты'].append(results)
+            return list(employees.values())
+        except Exception as e:
+            raise ValueError(f"Ошибка при чтении файла: {str(e)}")
 
     def __iter__(self):
         return self
@@ -65,3 +68,4 @@ class GetEmployee:
         else:
             self.wb.close()
             raise StopIteration
+
